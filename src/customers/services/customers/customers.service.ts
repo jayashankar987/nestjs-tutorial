051ec511c2e308e532494a526e346bcce6a50c33
jayashankar.boya <jayashankar.boya@byjus.com>
2022-07-30 00:30:58 +0530

    update changes as insisted by webstrom

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,25 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-
-    private users = [{
-        email: 'jayashankar987@gmail.com',
-            createdAt: new Date(),
-            id: 1
+  private users = [
+    {
+      email: 'jayashankar987@gmail.com',
+      createdAt: new Date(),
+      id: 1,
     },
     {
-        email: 'jayashankarboya@gmail.com',
-            createdAt: new Date(),
-            id: 2
+      email: 'jayashankarboya@gmail.com',
+      createdAt: new Date(),
+      id: 2,
     },
     {
-        email: 'pushparekha90@gmail.com',
-            createdAt: new Date(),
-            id: 3
-    }
-    ]
+      email: 'pushparekha90@gmail.com',
+      createdAt: new Date(),
+      id: 3,
+    },
+  ];
 
-    findCustomerById(id: number){
-        return this.users.find((user) => user.id === id)
-    }
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
 }
